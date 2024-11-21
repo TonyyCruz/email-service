b032fa350091725834a99206d3e3154ddd29e991
Anthony Cruz <tony_esqueloto@hotmail.com>
2024-11-21 16:48:24 -0300

    INITIAL COMMIT

---
+++ src/test/java/com/anthony/email_service/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthony.email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
